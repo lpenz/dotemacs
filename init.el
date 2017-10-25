a02b73862695681039b04529eccde4ceabc841d7
Leandro Lisboa Penz <lpenz@arista.com>
2017-10-25 10:47:43 +0100

    Initial setup with evil, magit and spacemacs-theme

---
+++ init.el
@@ -0,0 +1,20 @@
+; (package-initialize)
+
+(load "~/.emacs.d/my-packages.el")
+(add-hook 'after-init-hook '(lambda () (load "~/.emacs.d/my-afterinit.el")))
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" default)))
+ '(package-selected-packages (quote (use-package))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
