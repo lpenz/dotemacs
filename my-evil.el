5c9eb3c3436842cacbc4230cfe4bd159a7cc1e6f
Leandro Lisboa Penz <lpenz@lpenz.org>
2017-10-28 17:42:39 +0100

    evil: use standard functions, not helm

--- my-evil.el
+++ my-evil.el
@@ -6,12 +6,11 @@
   (define-key evil-insert-state-map (kbd "<f2>") 'save-buffer)
   (define-key evil-normal-state-map (kbd "<f2>") 'save-buffer)
   (define-key evil-normal-state-map (kbd "SPC TAB") 'evil-buffer)
-  (define-key evil-normal-state-map (kbd "SPC f f") 'helm-find-files)
-  (define-key evil-normal-state-map (kbd "SPC b b") 'helm-buffers-list)
+  (define-key evil-normal-state-map (kbd "SPC f f") 'find-file)
+  (define-key evil-normal-state-map (kbd "SPC b b") 'switch-to-buffer)
 
   (use-package evil-nerd-commenter
     :config
     (define-key evil-normal-state-map (kbd "SPC c l") 'evilnc-comment-or-uncomment-lines)
     (define-key evil-visual-state-map (kbd "SPC c l") 'evilnc-comment-or-uncomment-lines))
-  ;; :bind (("SPC c l" . evilnc-comment-or-uncomment-lines)))
   )
