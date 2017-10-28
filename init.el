b99dca7bfd513242fa35fa05d9717663a91066d6
Leandro Lisboa Penz <lpenz@lpenz.org>
2017-10-28 17:58:03 +0100

    use-package evil-magit

--- init.el
+++ init.el
@@ -6,6 +6,7 @@
 (load "~/.emacs.d/my-theme.el")
 (load "~/.emacs.d/my-evil.el")
 (use-package magit)
+(use-package evil-magit)
 (load "~/.emacs.d/my-autosave.el")
 
 ;; my-afterinit.el overrides whatever a rogue package may have done
