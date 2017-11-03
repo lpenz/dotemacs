c8d99673df9bb513331c9c223d67f309a186c2a5
Leandro Lisboa Penz <lpenz@arista.com>
2017-11-03 11:34:06 +0000

    Add fill-column-indicator

--- init.el
+++ init.el
@@ -7,6 +7,7 @@
 (load "~/.emacs.d/my-evil.el")
 (use-package magit)
 (use-package evil-magit)
+(use-package fill-column-indicator)
 (load "~/.emacs.d/my-ivy.el")
 (load "~/.emacs.d/my-autosave.el")
 (load "~/.emacs.d/my-projectile.el")
