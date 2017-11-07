197a54db78cbeff060ef032b3048ac8d208f6a38
Leandro Lisboa Penz <lpenz@lpenz.org>
2017-11-07 23:20:35 +0000

    SPC p f calls projectile-find-file

--- my-projectile.el
+++ my-projectile.el
@@ -4,6 +4,7 @@
   :config
   (setq projectile-completion-system 'ivy)
   (define-key evil-normal-state-map (kbd "SPC p l") 'projectile-switch-project)
+  (define-key evil-normal-state-map (kbd "SPC p f") 'projectile-find-file)
   (define-key evil-normal-state-map (kbd "<f10>") 'projectile-compile-project)
   (define-key evil-normal-state-map (kbd "SPC '") 'projectile-run-eshell)
   )
