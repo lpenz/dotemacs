fefe7399eea64642da71b771fd5c95305615646e
Leandro Lisboa Penz <lpenz@lpenz.org>
2017-11-07 23:15:10 +0000

    No longer use real-auto-save

--- my-autosave.el
+++ my-autosave.el
@@ -1,8 +1,8 @@
 ;; my-autosave.el
 
-(use-package real-auto-save)
-(add-hook 'text-mode-hook 'real-auto-save-mode)
-(add-hook 'prog-mode-hook 'real-auto-save-mode)
+;; (use-package real-auto-save)
+;; (add-hook 'text-mode-hook 'real-auto-save-mode)
+;; (add-hook 'prog-mode-hook 'real-auto-save-mode)
 (setq version-control 'never)
 (setq make-backup-files nil)
 
