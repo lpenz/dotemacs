26350bbc912cd0282e20c6fbce83b7e542bad0f6
Leandro Lisboa Penz <lpenz@arista.com>
2017-11-09 16:16:38 +0000

    SPC a = projectile-find-other-file

--- my-projectile.el
+++ my-projectile.el
@@ -3,6 +3,7 @@
 (use-package projectile
   :config
   (setq projectile-completion-system 'ivy)
+  (general-define-key "SPC a" 'projectile-find-other-file)
   (general-define-key "SPC p l" 'projectile-switch-project)
   (general-define-key "SPC p f" 'projectile-find-file)
   (general-define-key "<f10>" 'projectile-compile-project)
