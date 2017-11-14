f027ff535e9549169cc63c75bb67382d98d9d92a
Leandro Lisboa Penz <lpenz@arista.com>
2017-11-14 13:18:20 +0000

    Add C-w <arrow> key bindings

--- my-evil.el
+++ my-evil.el
@@ -4,7 +4,10 @@
   :config
   (evil-mode 1)
   (general-define-key "<f2>" 'save-buffer)
-  (general-define-key "<f2>" 'save-buffer)
+  (general-define-key "C-w <left>"  'evil-window-left)
+  (general-define-key "C-w <right>" 'evil-window-right)
+  (general-define-key "C-w <up>"    'evil-window-up)
+  (general-define-key "C-w <down>"  'evil-window-down)
   (general-define-key "SPC TAB" 'evil-buffer)
   ;; (general-define-key "SPC f f" 'find-file)
   (general-define-key "SPC b b" 'switch-to-buffer)
