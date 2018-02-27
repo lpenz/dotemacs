c58639c8343b4011c54f6c94fcf8212f53a65080
Leandro Lisboa Penz <lpenz@lpenz.org>
2018-02-27 23:47:16 +0000

    set-default-font Inconsolata

--- my-theme.el
+++ my-theme.el
@@ -5,3 +5,5 @@
   :init
   (setq spacemacs-theme-custom-colors '((green-bg-s . "#00ff00")))
   (load-theme 'spacemacs-dark t))
+
+(set-default-font "Inconsolata:size=19:antialias=true:autohint=false")
