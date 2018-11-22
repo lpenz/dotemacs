28cb98ebc7de9a569939d3c210165f9d42178977
Leandro Lisboa Penz <lpenz@lpenz.org>
2018-11-22 19:02:09 +0000

    my-counshell: spc e f = counshell-projectile-fd

--- my-counshell.el
+++ my-counshell.el
@@ -6,5 +6,6 @@
    (general-define-key "SPC e e" 'counshell-projectile-sh)
    (general-define-key "SPC e ]" 'counshell-projectile-gnuglobal)
    (general-define-key "SPC e s" 'counshell-projectile-rg)
+   (general-define-key "SPC e f" 'counshell-projectile-fd)
   )
 
