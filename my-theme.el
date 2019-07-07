02182b9d1d01790e095750606f8a52b94bb841d2
Leandro Lisboa Penz <lpenz@lpenz.org>
2019-07-07 11:45:47 +0100

    Sync GUI font and color with my terminal configuration

--- my-theme.el
+++ my-theme.el
@@ -3,7 +3,7 @@
 (use-package spacemacs-theme
   :defer t
   :init
-  (setq spacemacs-theme-custom-colors '((green-bg-s . "#00ff00")))
+  (setq spacemacs-theme-custom-colors '((green-bg-s . "#00ff00" ) (bg1 . "#262626")))
   (load-theme 'spacemacs-dark t))
 
-(set-default-font "Inconsolata:size=19:antialias=true:autohint=false")
+(set-default-font "InconsolataForPowerline Nerd Font:pixelsize=18")
