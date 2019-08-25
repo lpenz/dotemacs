ad5be408ece2641276514863b0681908acd694d1
Leandro Lisboa Penz <lpenz@lpenz.org>
2019-08-25 19:59:35 +0100

    Leave counshell development shortcut in my-counshell.el

--- my-counshell.el
+++ my-counshell.el
@@ -2,6 +2,7 @@
 
 (use-package counshell
   :quelpa ((counshell :fetcher github :repo "lpenz/counshell") :upgrade t)
+  ;; :quelpa ((counshell :fetcher file :path "/home/lpenz/projs/counshell/counshell.el") :upgrade t)
   :config
    (general-define-key "SPC e e" 'counshell-projectile-sh)
    (general-define-key "SPC e ]" 'counshell-projectile-gnuglobal)
