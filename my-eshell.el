eb841d821558692f4cb66bd7725cd03330a4c635
Leandro Lisboa Penz <lpenz@arista.com>
2020-04-06 16:13:15 +0100

    Key improvements: M-r has counsel-esh-history; SPC h h hightlight-*-point

--- my-eshell.el
+++ my-eshell.el
@@ -2,6 +2,7 @@
 
 (defun my-eshell-hook ()
   (general-define-key :keymaps 'eshell-mode-map "TAB" 'completion-at-point)
+  (general-define-key :keymaps 'eshell-mode-map "M-r" 'counsel-esh-history)
   (general-define-key :keymaps 'eshell-mode-map "C-c r" 'counsel-esh-history))
 
 (add-hook 'eshell-mode-hook 'my-eshell-hook)
--- my-keys.el
+++ my-keys.el
@@ -1,6 +1,7 @@
 ;; my-keys.el
 
 (general-define-key "SPC h h"  'highlight-symbol-at-point)
+(general-define-key "SPC h ."  'highlight-symbol-at-point)
 (general-define-key "SPC h r"  'highlight-regexp)
 (general-define-key "SPC h c"  'unhighlight-regexp)
 
