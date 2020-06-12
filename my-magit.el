9ff6a5f15967de866ef1bda88563e902f4c80f7b
Leandro Lisboa Penz <lpenz@lpenz.org>
2020-06-12 12:02:04 +0100

    Set magit-save-repository-buffers 'dontask

--- my-magit.el
+++ my-magit.el
@@ -1,6 +1,8 @@
 ;; my-magit.el
 
 (use-package magit
+  :init
+  (setq magit-save-repository-buffers 'dontask)
   :config
   (add-to-list 'magit-section-initial-visibility-alist '(unpulled . show))
   (add-to-list 'magit-section-initial-visibility-alist '(unpushed . show))
