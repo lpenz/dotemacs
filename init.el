357ff72b4b982c3d82b4d2c2fab9d27306f4b1bf
Leandro Lisboa Penz <lpenz@lpenz.org>
2021-08-02 09:14:40 +0100

    undo-tree init is global-undo-tree-mode

--- init.el
+++ init.el
@@ -16,7 +16,7 @@
 
   ;; Load files with individual packages and corresponding configs:
   (require 'my-usepackages)
-  (use-package undo-tree :init (undo-tree-mode))
+  (use-package undo-tree :init (global-undo-tree-mode))
   (require 'my-theme)
   (require 'my-general)
   (require 'my-evil)
