e97b9bf9eefe1fd0a123a640516865bbebbcbe83
Leandro Lisboa Penz <lpenz@lpenz.org>
2022-09-25 13:17:02 +0100

    my-flycheck.el: run flake8 via executable; disable mypy and pylint
    Running via the crazy default flycheck command-line was not working.
    
    mypy and pylint complain too much, and we can run pylint via flake8.

--- lisp/my-flycheck.el
+++ lisp/my-flycheck.el
@@ -4,7 +4,8 @@
   :init
   (global-flycheck-mode)
   :config
-  (setq-default flycheck-disabled-checkers '(emacs-lisp-checkdoc))
+  (setq-default flycheck-disabled-checkers '(emacs-lisp-checkdoc python-mypy python-pylint))
+  (setq-default flycheck-python-flake8-executable "/usr/bin/flake8")
   )
 
 (provide 'my-flycheck)
