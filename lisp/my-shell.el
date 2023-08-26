b074632cc8fd40711f9eeb354ec329f0ce4d0e71
Leandro Lisboa Penz <lpenz@lpenz.org>
2023-08-26 20:38:50 +0100

    Bind counsel-shell-history in shell mode

--- lisp/my-shell.el
+++ lisp/my-shell.el
@@ -8,5 +8,12 @@
   (setq explicit-bash-args
         (append '("--rcfile") bashrc '("-i"))))
 
+(defun my-shell-hook ()
+  (general-define-key :keymaps 'shell-mode-map "TAB" 'completion-at-point)
+  (general-define-key :keymaps 'shell-mode-map "M-r" 'counsel-shell-history)
+  (general-define-key :keymaps 'shell-mode-map "C-c r" 'counsel-shell-history))
+
+(add-hook 'shell-mode-hook 'my-shell-hook)
+
 (provide 'my-shell)
 
