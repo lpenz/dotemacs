fc1b9d1546852f36c97a2004613a2c0533fb5a3f
Leandro Lisboa Penz <lpenz@lpenz.org>
2023-09-24 18:05:55 +0100

    Fix which-key initialization, hopefully

--- lisp/my-whichkey.el
+++ lisp/my-whichkey.el
@@ -1,8 +1,11 @@
 ;; my-whichkey.el -*- lexical-binding: t; -*-
 
 (use-package which-key
+  :after evil
   :config
-  (which-key-mode))
+  (which-key-mode)
+  :custom
+  (which-key-idle-delay 0.7))
 
 (provide 'my-whichkey)
 
