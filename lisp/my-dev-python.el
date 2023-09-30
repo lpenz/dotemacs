445ae085bde6eeac815879662d48c94d552dedce
Leandro Lisboa Penz <lpenz@lpenz.org>
2023-09-30 19:32:54 +0100

    Python: don't use use-package's "commands" instruction
    Something was not working very well.

--- lisp/my-dev-python.el
+++ lisp/my-dev-python.el
@@ -1,16 +1,10 @@
 ;; my-dev-python.el -*- lexical-binding: t; -*-
 
 (use-package python-black
-  :hook python-mode
-  :commands (python-black-buffer
-             python-black-region
-             python-black-on-save-mode))
+  :hook python-mode)
 
 (use-package python-isort
-  :hook python-mode
-  :commands (python-isort-buffer
-             python-isort-region
-             python-isort-on-save-mode))
+  :hook python-mode)
 
 (add-to-list 'auto-mode-alist '("SConstruct" . python-mode))
 (add-to-list 'auto-mode-alist '("SConscript" . python-mode))
