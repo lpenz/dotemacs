b8c63c13732973fbc4b19fd48f9626873cb72395
Leandro Lisboa Penz <lpenz@lpenz.org>
2023-10-01 19:25:15 +0100

    Set safe-local-variable-values to allow black/isort to be loaded by dir-locals.el

--- lisp/my-dev-python.el
+++ lisp/my-dev-python.el
@@ -1,10 +1,16 @@
 ;; my-dev-python.el -*- lexical-binding: t; -*-
 
 (use-package python-black
-  :hook python-mode)
+  :demand t
+  :after python
+  :config
+  (add-to-list 'safe-local-variable-values (quote (eval (lambda nil (python-black-on-save-mode t))))))
 
 (use-package python-isort
-  :hook python-mode)
+  :demand t
+  :after python
+  :config
+  (add-to-list 'safe-local-variable-values (quote (eval (lambda nil (python-isort-on-save-mode t))))))
 
 (add-to-list 'auto-mode-alist '("SConstruct" . python-mode))
 (add-to-list 'auto-mode-alist '("SConscript" . python-mode))
--- lisp/my-flycheck.el
+++ lisp/my-flycheck.el
@@ -6,8 +6,8 @@
   :config
   (setq-default flycheck-disabled-checkers '(emacs-lisp-checkdoc python-mypy python-pylint))
   (setq-default flycheck-python-flake8-executable "/usr/bin/flake8")
-  (setq safe-local-variable-values (quote ((flycheck-gcc-language-standard . "c++17"))))
-  (setq safe-local-variable-values (quote ((flycheck-clang-language-standard . "c++17"))))
+  (add-to-list 'safe-local-variable-values (quote ((flycheck-gcc-language-standard . "c++17"))))
+  (add-to-list 'safe-local-variable-values (quote ((flycheck-clang-language-standard . "c++17"))))
   )
 
 (provide 'my-flycheck)
