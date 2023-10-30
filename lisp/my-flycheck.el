d2629c9c7f29f12e994f36eebb524e2a5a6e2c4d
Leandro Lisboa Penz <lpenz@lpenz.org>
2023-10-30 16:16:49 +0000

    lisp/my-flycheck.el: don't disable mypy by default

--- lisp/my-flycheck.el
+++ lisp/my-flycheck.el
@@ -4,7 +4,7 @@
   :init
   (global-flycheck-mode)
   :config
-  (setq-default flycheck-disabled-checkers '(emacs-lisp-checkdoc python-mypy python-pylint))
+  (setq-default flycheck-disabled-checkers '(emacs-lisp-checkdoc python-pylint))
   (setq-default flycheck-python-flake8-executable "/usr/bin/flake8")
   (add-to-list 'safe-local-variable-values (quote ((flycheck-gcc-language-standard . "c++17"))))
   (add-to-list 'safe-local-variable-values (quote ((flycheck-clang-language-standard . "c++17"))))
