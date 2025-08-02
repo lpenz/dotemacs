0bdcf618951aa239a9dd53420e1b952422054f8c
Your Name <you@example.com>
2025-08-02 22:21:57 +0100

    Fix GUI emacs font

--- lisp/my-theme.el
+++ lisp/my-theme.el
@@ -6,7 +6,10 @@
   (setq spacemacs-theme-custom-colors '((green-bg-s . "#00ff00" ) (bg1 . "#262626")))
   (load-theme 'spacemacs-dark t))
 
-(set-frame-font "MesloLGS NFM:size=14")
+;; (set-frame-font "Inconsolata-12")
+(set-frame-font "MesloLGS Nerd Font Mono-11")
+(set-fontset-font t 'unicode "Symbola" nil 'prepend)
+(set-fontset-font t 'unicode "Noto Color Emoji" nil 'prepend)
 
 (provide 'my-theme)
 
