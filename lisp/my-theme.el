5ae636bdb1bd80d5211e7e850e8081669d129e72
Leandro Lisboa Penz <lpenz@lpenz.org>
2025-08-03 22:06:34 +0100

    Fix some lint issues

--- lisp/my-theme.el
+++ lisp/my-theme.el
@@ -3,13 +3,16 @@
 (use-package spacemacs-theme
   :defer t
   :init
+  (defvar spacemacs-theme-custom-colors nil)
   (setq spacemacs-theme-custom-colors '((green-bg-s . "#00ff00" ) (bg1 . "#262626")))
   (load-theme 'spacemacs-dark t))
 
-;; (set-frame-font "Inconsolata-12")
-(set-frame-font "MesloLGS Nerd Font Mono-11")
-(set-fontset-font t 'unicode "Symbola" nil 'prepend)
-(set-fontset-font t 'unicode "Noto Color Emoji" nil 'prepend)
+
+(when (display-graphic-p)
+  ;; (set-frame-font "Inconsolata-12")
+  (set-frame-font "MesloLGS Nerd Font Mono-11")
+  ;; (set-fontset-font t 'unicode "Symbola" nil 'prepend)
+  (set-fontset-font t 'unicode "Noto Color Emoji" nil 'prepend))
 
 (provide 'my-theme)
 
