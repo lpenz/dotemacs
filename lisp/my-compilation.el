9e4d33ef5bdf26eb14996bd2d08b58b0042401fd
Your Name <you@example.com>
2025-08-03 22:36:16 +0100

    my-compilation.el: tail compilation, ansi support

---
+++ lisp/my-compilation.el
@@ -0,0 +1,19 @@
+;; my-compilation.el -*- lexical-binding: t; -*-
+
+;; Make compilation buffer tail by default
+
+(with-eval-after-load 'compile
+  ;; set cursor to follow compilation output
+  (setq compilation-scroll-output t))
+
+;; Add ansi support to compilation buffer:
+
+(require 'ansi-color)
+
+(defun colorize-compilation-buffer ()
+  (let ((inhibit-read-only t))
+    (ansi-color-apply-on-region (point-min) (point-max))))
+
+(add-hook 'compilation-filter-hook 'colorize-compilation-buffer)
+
+(provide 'my-compilation)
