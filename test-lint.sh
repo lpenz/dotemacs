ac06b823e2ab89a96d3d07835ade8bdd8d38a212
Leandro Lisboa Penz <lpenz@lpenz.org>
2026-04-18 20:49:16 +0100

    ci: enhance test-lint.sh with relint and checkdoc

--- test-lint.sh
+++ test-lint.sh
@@ -13,7 +13,21 @@ emacs --batch \
                   (load-path (delq default-directory load-path)))
                  (load-file user-init-file)
                  (run-hooks (quote after-init-hook))
-                 (let ((byte-compile-error-on-warn t))
-                   (dolist (file (directory-files "lisp" t "\\.el$"))
+                 (let ((byte-compile-error-on-warn t)
+                       (files (directory-files "lisp" t "\\.el$")))
+                   ;; 1. Byte-compile check
+                   (dolist (file files)
                      (message "Byte-compiling %s..." file)
-                     (byte-compile-file file)))))'
+                     (byte-compile-file file))
+
+                   ;; 2. Relint check
+                   (straight-use-package (quote relint))
+                   (require (quote relint))
+                   (dolist (file files)
+                     (message "Relinting %s..." file)
+                     (relint-file file))
+
+                   ;; 3. Checkdoc check
+                   (dolist (file files)
+                     (message "Checking documentation in %s..." file)
+                     (checkdoc-file file)))))'
