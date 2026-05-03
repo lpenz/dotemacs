a0f25f649ab1b56085510ba901fe8847cc9dc355
Leandro Lisboa Penz <lpenz@lpenz.org>
2026-05-03 15:57:14 +0100

    fix(marginalia): move mode activation to :config to fix initialization
    Calling marginalia-mode in :init caused void-function errors in batch
    mode during linting because the package wasn't loaded yet. Moving it to
    :config and adding :demand t ensures the package is available before
    the function is called.

--- lisp/my-vertico.el
+++ lisp/my-vertico.el
@@ -100,11 +100,12 @@
 
 ;; Enable rich annotations using the Marginalia package
 (use-package marginalia
+  :demand t
   ;; Bind `marginalia-cycle' locally in the minibuffer.  To make the binding
   ;; available in the *Completions* buffer, add it to the
   ;; `completion-list-mode-map'.
   :bind (:map minibuffer-local-map ("M-A" . marginalia-cycle))
-  :init
+  :config
   (marginalia-mode))
 
 (use-package embark
